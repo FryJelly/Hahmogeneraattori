777453e8facc8587612c06249bb01698352dc52c
Marko Sydänmaa <skyris@hotmail.fi>
2018-04-24 00:09:44 +0300

    Tried to make races stat mods to work

--- Hahmogeneraattori/Hahmogeneraattori/Hahmogeneraattori/Views/CharCreation.xaml.cs
+++ Hahmogeneraattori/Hahmogeneraattori/Hahmogeneraattori/Views/CharCreation.xaml.cs
@@ -32,6 +32,44 @@ namespace Hahmogeneraattori.Views
             dgStats.ItemsSource = Stats.RollStats();
         }
 
+        private void ListBox_SelectionChanged()
+        {
 
+        }
+
+        private void Dwarf_Selected(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Elf_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Gnome_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HalfElf_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HalfOrc_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Halfling_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Human_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
